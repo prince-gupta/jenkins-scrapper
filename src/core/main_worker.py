5d71dcd47b1c36f4a00b2fa15c8f9ef88b87d473
Prince Gupta <princegupta@Princes-MacBook.local>
2018-03-20 00:09:50 +0530

    Added Changes for adding gpio in main job and made it optional

--- src/core/main_worker.py
+++ src/core/main_worker.py
@@ -1,7 +1,8 @@
 from src.j_api.jenkins_server import JenkinsServer
 from src.j_api.job_workers import JobWorkers
 from src.properties_util import PropertyUtil
-from src.gpio_api.led_status import LEDStatus
+if PropertyUtil().get('core', 'enable_gpio') is True:
+    from src.gpio_api.led_status import LEDStatus
 
 from time import sleep
 
@@ -10,18 +11,17 @@ class MainWorker:
     __jenkins_server__: JenkinsServer
     __job_worker__: JobWorkers
 
-    def __setup_jenkins_server__(self):
+    def __init__(self):
         MainWorker.__jenkins_server__ = JenkinsServer().get_server()
-
-    def __setup_job_worker(self):
         MainWorker.__job_worker__ = JobWorkers(MainWorker.__jenkins_server__, PropertyUtil().get('core', 'job'))
 
     def start(self):
         while True:
             status = MainWorker.__job_worker__.should_raise_alarm()
-            if status is True:
-                LEDStatus().mark_fail()
-            else:
-                LEDStatus().mark_success()
+            if PropertyUtil().get('core', 'enable_gpio') is True:
+                if status is True:
+                    LEDStatus().mark_fail()
+                else:
+                    LEDStatus().mark_success()
 
         sleep(PropertyUtil().get('core', 'refresh_rate'))
--- src/main.py
+++ src/main.py
@@ -1,11 +1,2 @@
-from src.j_api.jenkins_server import JenkinsServer
-from src.j_api.job_workers import JobWorkers
-from time import sleep
-from src.properties_util import PropertyUtil
-
-jenkins = JenkinsServer().get_server()
-job = JobWorkers(jenkins, 'python-practice')
-
-while True:
-    print(job.should_raise_alarm())
-    sleep(PropertyUtil().get('core', 'refresh_rate'))
+from src.core.main_worker import MainWorker
+MainWorker().start()
