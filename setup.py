ab41925a01a30e7435d9ddeb5a5be9524a5bd042
Prince Gupta <princegupta@Princes-MacBook.local>
2018-03-20 02:12:13 +0530

    Changes in Setup file.

--- setup.py
+++ setup.py
@@ -19,9 +19,13 @@ from setuptools import setup
 setup(
     name='jenkins-scrapper',
     version='1.0',
-    packages=['src', 'src.core', 'src.j_api', 'src.gpio_api', 'PyYAML', 'certifi', 'chardet', 'gpiozero', 'idna',
-              'jenkinsapi', 'pip', 'pytz', 'requests', 'setuptools', 'ruamel.appconfig', 'ruamel.std.argparse', 'six',
-              'urllib3'],
+    packages=['src', 'src.core', 'src.j_api', 'src.gpio_api'],
+
+    install_requires=[
+        'PyYAML', 'certifi', 'chardet', 'gpiozero', 'idna',
+        'jenkinsapi', 'pip', 'pytz', 'requests', 'setuptools', 'ruamel.appconfig', 'ruamel.std.argparse', 'six',
+        'urllib3'],
+    
     url='',
     license='',
     author='princegupta',
